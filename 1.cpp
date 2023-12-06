f65e0d8c47760384fc03f6ae7089219773204b30
Toufeeqmir <122149711+Toufeeqmir@users.noreply.github.com>
2023-12-06 20:40:06 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int n;
+    cout <<"Enter the value of n "<<endl;
+    cin>>n;
+    while(n<=10){
+        cout<<n<<endl;;
+        cout<<n*n*n<<endl;
+        n++;
+    }
+}
